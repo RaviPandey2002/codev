675874fc3e8767e706481d4ee984b571b239d509
ravi <ravi2001pandey@gmail.com>
2026-07-23 01:42:06 +0530

    Chore: configured the client moudule and install shadcn-ui library , created Login and RegisterPage

---
+++ client/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import './index.css'
+
+import LoginPage from '@/pages/LoginPage'
+import RegisterPage from '@/pages/RegisterPage'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
