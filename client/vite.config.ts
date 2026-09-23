bd1b446dbbb99318aed17159cd7620a5eefc366d
ravi <ravi2001pandey@gmail.com>
2026-09-23 23:30:58 +0530

    fix: add type module to shared and resolve y-monaco editor api in vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'monaco-editor/esm/vs/editor/editor.api.js': 'monaco-editor',
     },
   },
 })
